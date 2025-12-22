290bb57152e217eba1d18ff49475691c1b306855
lazz <134055367+muhammadlazz@users.noreply.github.com>
2025-12-22 17:13:51 +0700

    Update and rename draft2-withoutblynk.cpp to draft2-withoutblynk.ino

--- draft2-withoutblynk.ino
+++ draft2-withoutblynk.ino
@@ -1,14 +1,9 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
-
-// --- LIBRARY SERVO ---
 #include <ESP32Servo.h>       
-
-// --- LIBRARY SUHU (DIAKTIFKAN) ---
 #include <OneWire.h>          
 #include <DallasTemperature.h>
 
-// --- 1. PENGATURAN PIN ---
 #define I2C_SDA 21  
 #define I2C_SCL 22  
 
@@ -18,32 +13,25 @@
 #define relayPin 23
 #define servoPin 14  
 
-// Pin Sensor Suhu (DIAKTIFKAN)
 #define oneWireBus 27 
 
-// --- 2. INISIALISASI OBJEK ---
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 Servo pakanServo; 
 
-// Objek Suhu (DIAKTIFKAN)
 OneWire oneWire(oneWireBus); 
 DallasTemperature sensors(&oneWire); 
 
-// --- 3. KONFIGURASI RELAY & LDR ---
 const int KONDISI_GELAP = HIGH; 
 const int KONDISI_TERANG = LOW;
 const int RELAY_NYALA = LOW; 
 const int RELAY_MATI = HIGH;
 
-// --- 4. PENGATURAN PAKAN OTOMATIS ---
 unsigned long previousMillis = 0;
-// Atur jadwal makan di sini (contoh: 10 detik untuk tes)
 const long intervalPakan = 10000; 
 
 void setup() {
   Serial.begin(115200);
 
-  // --- Init LCD ---
   Wire.begin(I2C_SDA, I2C_SCL);
   lcd.init();
   lcd.backlight();
@@ -55,36 +43,30 @@ void setup() {
   delay(1000);
   lcd.clear();
 
-  // --- Init Sensor & Actuator ---
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(ldrPin, INPUT);
   pinMode(relayPin, OUTPUT);
   digitalWrite(relayPin, RELAY_MATI); 
 
-  // --- Init Servo ---
   ESP32PWM::allocateTimer(0);
   ESP32PWM::allocateTimer(1);
   ESP32PWM::allocateTimer(2);
   ESP32PWM::allocateTimer(3);
   pakanServo.setPeriodHertz(50); 
   pakanServo.attach(servoPin, 500, 2400); 
-  pakanServo.write(0); // Posisi awal tertutup
+  pakanServo.write(0); 
 
-  // --- Init Suhu (DIAKTIFKAN) ---
   sensors.begin();
 }
 
 void loop() {
-  // --- A. BACA SENSOR ---
   float jarak = ukurJarakCm();
   int statusLDR = digitalRead(ldrPin);
   
-  // Baca Suhu (DIAKTIFKAN)
   sensors.requestTemperatures(); 
   float suhuAir = sensors.getTempCByIndex(0);
 
-  // --- B. LOGIKA LAMPU (RELAY) ---
   String statusLampuStr = "";
   if (statusLDR == KONDISI_GELAP) {
     digitalWrite(relayPin, RELAY_NYALA);
@@ -94,53 +76,42 @@ void loop() {
     statusLampuStr = "OFF";
   }
 
-  // --- C. LOGIKA PAKAN OTOMATIS ---
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= intervalPakan) {
     previousMillis = currentMillis;
     beriPakanIkan(); 
   }
 
-  // --- D. TAMPILAN LCD (LAYOUT BARU) ---
-  // Agar muat, kita singkat teksnya.
-  
-  // Baris 1: Suhu & Lampu
-  // Tampilan: "T:28C Lamp:ON "
   lcd.setCursor(0, 0);
   lcd.print("T:"); 
   
-  // Cek error sensor suhu (-127 artinya error/kabel lepas)
   if(suhuAir == -127.00) {
     lcd.print("Err"); 
   } else {
-    lcd.print(suhuAir, 0); // Tampilkan suhu bulat (tanpa koma) hemat tempat
-    lcd.print((char)223);  // Simbol derajat
+    lcd.print(suhuAir, 0); 
+    lcd.print((char)223);  
     lcd.print("C");
   }
 
   lcd.print(" Lmp:"); 
   lcd.print(statusLampuStr);
-  lcd.print(" "); // Spasi pembersih sisa karakter
+  lcd.print(" "); 
 
-  // Baris 2: Sisa Pakan (Jarak)
   lcd.setCursor(0, 1);
   lcd.print("Pakan: ");
-  if (jarak > 0 && jarak < 100) { // Filter angka aneh
+  if (jarak > 0 && jarak < 100) { 
     lcd.print(jarak, 0);
     lcd.print("cm    "); 
   } else {
     lcd.print("Error ");
   }
 
-  // Debugging Serial Monitor
   Serial.print("Suhu: "); Serial.print(suhuAir);
   Serial.print("C | Jarak: "); Serial.print(jarak);
   Serial.print("cm | Lampu: "); Serial.println(statusLampuStr);
 
   delay(1000); 
 }
-
-// --- FUNGSI TAMBAHAN ---
 
 void beriPakanIkan() {
   Serial.println("--- WAKTUNYA MAKAN! ---");
